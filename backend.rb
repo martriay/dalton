0d6a247c97a8988a545a5c3e2388e5ccd87c834a
Martín Triay <martriay@gmail.com>
2013-02-26 16:31:03 -0300

    Update backend.rb

--- backend.rb
+++ backend.rb
@@ -4,6 +4,10 @@ require "sinatra"
 set :port, 80
 set :views, '.'
 
+get '/' do
+  haml :nohex 
+end
+
 get '/:hex' do
   display = params[:hex] =~ /^#?([a-f0-9]{6}|[a-f0-9]{3})$/ ? :hex : :nohex
   haml display, :locals => { :color => params[:hex] } 
